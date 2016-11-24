666e6a6f18f5d5d5b28a46e0af3de3cc9d5472f2
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2016-11-23 22:17:02 -0800

    Setting things up

---
+++ WebJobsLanguageService/WebJobs.Script.LanguageService/LanguageServiceConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hosting;
+
+namespace WebJobs.Script.LanguageService
+{
+    public class LanguageServiceConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            return Connection.Send(connectionId, "connected");
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+    }
+}
---
+++ WebJobsLanguageService/WebJobs.Script.LanguageService/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebJobs.Script.LanguageService.Startup))]
+
+namespace WebJobs.Script.LanguageService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<LanguageServiceConnection>("/ls");
+        }
+    }
+}
